767bd7a8d49b49b5addc3d5c7466ee091c2fd003
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2023-12-08 14:11:03 +0100

    Add day7 part two

--- day7/main.go
+++ day7/main.go
@@ -54,20 +54,39 @@ func (o Occurences) GetOccurencesType() Type {
 
 }
 
-var Strength = map[string]int{
-	"A": 13,
-	"K": 12,
-	"Q": 11,
-	"J": 10,
-	"T": 9,
-	"9": 8,
-	"8": 7,
-	"7": 6,
-	"6": 5,
-	"5": 4,
-	"4": 3,
-	"3": 2,
-	"2": 1,
+func (o Occurences) GetOccurencesTypeJoker() Type {
+	if len(o) == 1 ||
+		(len(o) == 2 && o["J"] > 0) {
+		return FiveOfAKind
+	}
+	for label, occurrence := range o {
+		if occurrence == 4 ||
+			(occurrence == 3 && o["J"] > 0) ||
+			(label != "J" && occurrence == 2 && o["J"] == 2) {
+			return FourOfAKind
+		}
+	}
+	if len(o) == 2 ||
+		(len(o) == 3 && o["J"] > 0) {
+		return FullHouse
+	}
+	for _, occurrence := range o {
+		if occurrence == 3 ||
+			(occurrence == 2 && o["J"] > 0) {
+			return ThreeOfAKind
+		}
+	}
+	if len(o) == 3 ||
+		(len(o) == 4 && o["J"] > 0) {
+		return TwoPair
+	}
+	for _, occurrence := range o {
+		if occurrence == 2 || o["J"] > 0 {
+			return OnePair
+		}
+	}
+	return HighCard
+
 }
 
 type Card struct {
@@ -112,6 +131,22 @@ func (h Hands) ComputeTotalWinnings() int {
 }
 
 func parseHands(input []string) Hands {
+	var Strength = map[string]int{
+		"A": 13,
+		"K": 12,
+		"Q": 11,
+		"J": 10,
+		"T": 9,
+		"9": 8,
+		"8": 7,
+		"7": 6,
+		"6": 5,
+		"5": 4,
+		"4": 3,
+		"3": 2,
+		"2": 1,
+	}
+
 	hands := make(Hands, 0)
 	for _, line := range input {
 		cards := strings.Fields(line)
@@ -139,9 +174,56 @@ func parseHands(input []string) Hands {
 	return hands
 }
 
+func parseHandsJoker(input []string) Hands {
+	var StrengthJoker = map[string]int{
+		"A": 13,
+		"K": 12,
+		"Q": 11,
+		"T": 10,
+		"9": 9,
+		"8": 8,
+		"7": 7,
+		"6": 6,
+		"5": 5,
+		"4": 4,
+		"3": 3,
+		"2": 2,
+		"J": 1,
+	}
+
+	hands := make(Hands, 0)
+	for _, line := range input {
+		cards := strings.Fields(line)
+		bid, err := strconv.Atoi(cards[1])
+		if err != nil {
+			panic(err)
+		}
+		hand := Hand{Bid: bid}
+		occurrences := make(Occurences)
+		for _, card := range cards[0] {
+			card := string(card)
+			hand.Cards = append(hand.Cards, Card{
+				Value:    card,
+				Strength: StrengthJoker[card],
+			})
+			if occurrence, ok := occurrences[card]; ok {
+				occurrences[card] = occurrence + 1
+			} else {
+				occurrences[card] = 1
+			}
+		}
+		hand.Type = occurrences.GetOccurencesTypeJoker()
+		hands = append(hands, hand)
+	}
+	return hands
+}
+
 func main() {
 	input := utils.ReadFileByLine("day7/input.txt")
 	hands := parseHands(input)
 	hands.SortDesc()
 	fmt.Println("Part 1:", hands.ComputeTotalWinnings())
+	hands = parseHandsJoker(input)
+	hands.SortDesc()
+	fmt.Println("Part 2:", hands.ComputeTotalWinnings())
 }
--- day7/main_test.go
+++ day7/main_test.go
@@ -5,78 +5,7 @@ import (
 	"testing"
 )
 
-func TestParseHands(t *testing.T) {
-	input := []string{
-		"32T3K 765",
-		"T55J5 684",
-		"KK677 28",
-		"KTJJT 220",
-		"QQQJA 483",
-	}
-	expected := Hands{
-		{
-			Cards: []Card{
-				{Value: "3", Strength: 2},
-				{Value: "2", Strength: 1},
-				{Value: "T", Strength: 9},
-				{Value: "3", Strength: 2},
-				{Value: "K", Strength: 12},
-			},
-			Bid:  765,
-			Type: OnePair,
-		},
-		{
-			Cards: []Card{
-				{Value: "T", Strength: 9},
-				{Value: "5", Strength: 4},
-				{Value: "5", Strength: 4},
-				{Value: "J", Strength: 10},
-				{Value: "5", Strength: 4},
-			},
-			Bid:  684,
-			Type: ThreeOfAKind,
-		},
-		{
-			Cards: []Card{
-				{Value: "K", Strength: 12},
-				{Value: "K", Strength: 12},
-				{Value: "6", Strength: 5},
-				{Value: "7", Strength: 6},
-				{Value: "7", Strength: 6},
-			},
-			Bid:  28,
-			Type: TwoPair,
-		},
-		{
-			Cards: []Card{
-				{Value: "K", Strength: 12},
-				{Value: "T", Strength: 9},
-				{Value: "J", Strength: 10},
-				{Value: "J", Strength: 10},
-				{Value: "T", Strength: 9},
-			},
-			Bid:  220,
-			Type: TwoPair,
-		},
-		{
-			Cards: []Card{
-				{Value: "Q", Strength: 11},
-				{Value: "Q", Strength: 11},
-				{Value: "Q", Strength: 11},
-				{Value: "J", Strength: 10},
-				{Value: "A", Strength: 13},
-			},
-			Bid:  483,
-			Type: ThreeOfAKind,
-		},
-	}
-	actual := parseHands(input)
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected %v, got %v", expected, actual)
-	}
-}
-
-func TestSortHands(t *testing.T) {
+func TestParseAndSortHands(t *testing.T) {
 	input := []string{
 		"32T3K 765",
 		"T55J5 684",
@@ -207,6 +136,144 @@ func TestComputeTotalWinnings(t *testing.T) {
 		},
 	}
 	expected := 6440
+	input.SortDesc()
+	actual := input.ComputeTotalWinnings()
+	if actual != expected {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestParseAndSortHandsJoker(t *testing.T) {
+	input := []string{
+		"32T3K 765",
+		"T55J5 684",
+		"KK677 28",
+		"KTJJT 220",
+		"QQQJA 483",
+	}
+	expected := Hands{
+		{
+			Cards: []Card{
+				{Value: "3", Strength: 3},
+				{Value: "2", Strength: 2},
+				{Value: "T", Strength: 10},
+				{Value: "3", Strength: 3},
+				{Value: "K", Strength: 12},
+			},
+			Bid:  765,
+			Type: OnePair,
+		},
+		{
+			Cards: []Card{
+				{Value: "K", Strength: 12},
+				{Value: "K", Strength: 12},
+				{Value: "6", Strength: 6},
+				{Value: "7", Strength: 7},
+				{Value: "7", Strength: 7},
+			},
+			Bid:  28,
+			Type: TwoPair,
+		},
+		{
+			Cards: []Card{
+				{Value: "T", Strength: 10},
+				{Value: "5", Strength: 5},
+				{Value: "5", Strength: 5},
+				{Value: "J", Strength: 1},
+				{Value: "5", Strength: 5},
+			},
+			Bid:  684,
+			Type: FourOfAKind,
+		},
+		{
+			Cards: []Card{
+				{Value: "Q", Strength: 11},
+				{Value: "Q", Strength: 11},
+				{Value: "Q", Strength: 11},
+				{Value: "J", Strength: 1},
+				{Value: "A", Strength: 13},
+			},
+			Bid:  483,
+			Type: FourOfAKind,
+		},
+		{
+			Cards: []Card{
+				{Value: "K", Strength: 12},
+				{Value: "T", Strength: 10},
+				{Value: "J", Strength: 1},
+				{Value: "J", Strength: 1},
+				{Value: "T", Strength: 10},
+			},
+			Bid:  220,
+			Type: FourOfAKind,
+		},
+	}
+	actual := parseHandsJoker(input)
+	actual.SortDesc()
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, got %v", expected, actual)
+	}
+}
+
+func TestComputeTotalWinningsJoker(t *testing.T) {
+	input := Hands{
+		{
+			Cards: []Card{
+				{Value: "3", Strength: 3},
+				{Value: "2", Strength: 2},
+				{Value: "T", Strength: 10},
+				{Value: "3", Strength: 3},
+				{Value: "K", Strength: 12},
+			},
+			Bid:  765,
+			Type: OnePair,
+		},
+		{
+			Cards: []Card{
+				{Value: "K", Strength: 12},
+				{Value: "K", Strength: 12},
+				{Value: "6", Strength: 6},
+				{Value: "7", Strength: 7},
+				{Value: "7", Strength: 7},
+			},
+			Bid:  28,
+			Type: TwoPair,
+		},
+		{
+			Cards: []Card{
+				{Value: "T", Strength: 10},
+				{Value: "5", Strength: 5},
+				{Value: "5", Strength: 5},
+				{Value: "J", Strength: 1},
+				{Value: "5", Strength: 5},
+			},
+			Bid:  684,
+			Type: FourOfAKind,
+		},
+		{
+			Cards: []Card{
+				{Value: "Q", Strength: 11},
+				{Value: "Q", Strength: 11},
+				{Value: "Q", Strength: 11},
+				{Value: "J", Strength: 1},
+				{Value: "A", Strength: 13},
+			},
+			Bid:  483,
+			Type: FourOfAKind,
+		},
+		{
+			Cards: []Card{
+				{Value: "K", Strength: 12},
+				{Value: "T", Strength: 10},
+				{Value: "J", Strength: 1},
+				{Value: "J", Strength: 1},
+				{Value: "T", Strength: 10},
+			},
+			Bid:  220,
+			Type: FourOfAKind,
+		},
+	}
+	expected := 5905
 	input.SortDesc()
 	actual := input.ComputeTotalWinnings()
 	if actual != expected {
