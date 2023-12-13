2a1cfdf81c5874f0bbd5f12ab58004e1d30313ae
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2023-12-13 01:15:27 +0100

    Add day10 part two

--- day10/main.go
+++ day10/main.go
@@ -27,26 +27,32 @@ var (
 	}
 )
 
-type Tiles [][]int
+type Tile struct {
+	Value  int
+	Border bool
+}
+
+type Tiles [][]Tile
 
 func isStartingPoint(pipe int) bool {
 	return pipe&(North|South|East|West) == North|South|East|West
 }
 
 func (t Tiles) Move(y, x, count, from int) int {
-	if isStartingPoint(t[y][x]) && count > 0 {
+	t[y][x].Border = true
+	if isStartingPoint(t[y][x].Value) && from != North|South|East|West {
 		return count
 	}
-	if from != South && t[y][x]&North != 0 && t[y-1][x]&South != 0 {
+	if from != South && t[y][x].Value&North != 0 && t[y-1][x].Value&South != 0 {
 		return t.Move(y-1, x, count+1, North)
 	}
-	if from != North && t[y][x]&South != 0 && t[y+1][x]&North != 0 {
+	if from != North && t[y][x].Value&South != 0 && t[y+1][x].Value&North != 0 {
 		return t.Move(y+1, x, count+1, South)
 	}
-	if from != West && t[y][x]&East != 0 && t[y][x+1]&West != 0 {
+	if from != West && t[y][x].Value&East != 0 && t[y][x+1].Value&West != 0 {
 		return t.Move(y, x+1, count+1, East)
 	}
-	if from != East && t[y][x]&West != 0 && t[y][x-1]&East != 0 {
+	if from != East && t[y][x].Value&West != 0 && t[y][x-1].Value&East != 0 {
 		return t.Move(y, x-1, count+1, West)
 	}
 	return 0
@@ -55,7 +61,7 @@ func (t Tiles) Move(y, x, count, from int) int {
 func (t Tiles) ComputeFarthestStep() int {
 	for y, pipes := range t {
 		for x, pipe := range pipes {
-			if isStartingPoint(pipe) {
+			if isStartingPoint(pipe.Value) {
 				return t.Move(y, x, 0, North|South|East|West) / 2
 			}
 		}
@@ -63,13 +69,53 @@ func (t Tiles) ComputeFarthestStep() int {
 	return 0
 }
 
+func (t Tiles) Dump() {
+	count := 0
+	for _, pipes := range t {
+		inside := false
+		for _, pipe := range pipes {
+			if pipe.Border {
+				if isStartingPoint(pipe.Value) {
+					fmt.Print("░")
+					inside = !inside
+				} else if pipe.Value&North != 0 && pipe.Value&South != 0 {
+					fmt.Print("│")
+					inside = !inside
+				} else if pipe.Value&East != 0 && pipe.Value&West != 0 {
+					fmt.Print("─")
+				} else if pipe.Value&North != 0 && pipe.Value&East != 0 {
+					fmt.Print("└")
+					inside = !inside
+				} else if pipe.Value&North != 0 && pipe.Value&West != 0 {
+					fmt.Print("┘")
+					inside = !inside
+				} else if pipe.Value&South != 0 && pipe.Value&West != 0 {
+					fmt.Print("┐")
+					// inside = !inside
+				} else if pipe.Value&South != 0 && pipe.Value&East != 0 {
+					fmt.Print("┌")
+					// inside = !inside
+				}
+			} else {
+				if inside {
+					count++
+					fmt.Print("█")
+				} else {
+					fmt.Print(" ")
+				}
+			}
+		}
+		fmt.Println()
+	}
+	fmt.Println("Part 2", count)
+}
 func parseTiles(input []string) Tiles {
-	tiles := make([][]int, len(input))
+	tiles := make([][]Tile, len(input))
 	for y, line := range input {
-		currentTiles := make([]int, len(line))
+		currentTiles := make([]Tile, len(line))
 		for x, char := range line {
 			char := string(char)
-			currentTiles[x] = Connections[char]
+			currentTiles[x].Value = Connections[char]
 		}
 		tiles[y] = currentTiles
 	}
@@ -80,5 +126,5 @@ func main() {
 	input := utils.ReadFileByLine("input.txt")
 	tiles := parseTiles(input)
 	fmt.Println("Part 1:", tiles.ComputeFarthestStep())
-	fmt.Println("Part 2:")
+	tiles.Dump()
 }
