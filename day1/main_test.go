abcc9ce1692497db6942da921b9b132578e471b7
Quentin Selle <62110608+quentinselle@users.noreply.github.com>
2024-11-30 15:53:43 +0100

    Fix benchmark values

--- day1/main_test.go
+++ day1/main_test.go
@@ -37,12 +37,10 @@ func TestSolvePartTwo(t *testing.T) {
 
 func BenchmarkSolvePartOne(b *testing.B) {
 	input := []string{
-		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-		"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-		"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+		"1abc2",
+		"pqr3stu8vwx",
+		"a1b2c3d4e5f",
+		"treb7uchet",
 	}
 	for i := 0; i < b.N; i++ {
 		solvePartOne(input)
@@ -51,12 +49,13 @@ func BenchmarkSolvePartOne(b *testing.B) {
 
 func BenchmarkSolvePartTwo(b *testing.B) {
 	input := []string{
-		"Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53",
-		"Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19",
-		"Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1",
-		"Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83",
-		"Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36",
-		"Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11",
+		"two1nine",
+		"eightwothree",
+		"abcone2threexyz",
+		"xtwone3four",
+		"4nineeightseven2",
+		"zoneight234",
+		"7pqrstsixteen",
 	}
 	for i := 0; i < b.N; i++ {
 		solvePartTwo(input)
